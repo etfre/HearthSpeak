64b2e902f5c20c53c85c6c5e74f5fc392d6166d2
Evan <evfredericksen@gmail.com>
2018-08-19 23:37:07 -0400

    remove unused imports

--- HearthSpeak/Config.cs
+++ HearthSpeak/Config.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IniParser;
 
 namespace HearthSpeak
 {
